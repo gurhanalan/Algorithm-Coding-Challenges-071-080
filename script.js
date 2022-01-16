c10a40eacfd091003563f247975366a46660f61c
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-16 10:19:26 +0300

    Add challange

--- script.js
+++ script.js
@@ -109,3 +109,76 @@ function addTogether() {
 }
 
 console.log(addTogether(2)(3));
+
+// 74. Make a Person - freecodecamp
+
+/* Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object. */
+// +++++++++++++++
+/* Object.keys(bob).length should return 6.
+
+bob instanceof Person should return true.
+
+bob.firstName should return undefined.
+
+bob.lastName should return undefined.
+
+bob.getFirstName() should return the string Bob.
+
+bob.getLastName() should return the string Ross.
+
+bob.getFullName() should return the string Bob Ross.
+
+bob.getFullName() should return the string Haskell Ross after bob.setFirstName("Haskell").
+
+bob.getFullName() should return the string Haskell Curry after bob.setLastName("Curry").
+
+bob.getFullName() should return the string Haskell Curry after bob.setFullName("Haskell Curry").
+
+bob.getFirstName() should return the string Haskell after bob.setFullName("Haskell Curry").
+
+bob.getLastName() should return the string Curry after bob.setFullName("Haskell Curry"). */
+
+const Person = function (firstAndLast) {
+    // Only change code below this line
+    // Complete the method below and implement the others similarly
+    let [first, last] = firstAndLast.split(" ");
+    // console.log(first, "and", last);
+    this.getFullName = function () {
+        return first + " " + last;
+    };
+    this.getFirstName = function () {
+        return first;
+    };
+    this.getLastName = function () {
+        return last;
+    };
+    this.setLastName = function (name) {
+        last = name;
+    };
+    this.setFirstName = function (name) {
+        first = name;
+    };
+    this.setFullName = function (name) {
+        [first, last] = name.split(" ");
+    };
+    return firstAndLast;
+};
+
+const bob = new Person("Bob Ross");
+
+console.log(bob.getFullName());
+console.log(bob.getFirstName());
+bob.setLastName("mike");
+console.log(bob.getLastName());
+console.log(bob.getFullName());
+bob.setFullName("alice spencer");
+console.log(bob.getFullName());
+console.log(Object.keys(bob));

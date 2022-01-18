e2453b59e905629d40dda93eead1cfa1eb96ce9d
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-01-18 10:36:43 +0300

    Add challange

--- script.js
+++ script.js
@@ -228,3 +228,91 @@ function palindrome2(str) {
 console.log(palindrome2("My age is 0, 0 si ega ym.")); //true
 console.log(palindrome2("__My age is-0, 0 s  i- ega ym.")); //true
 console.log(palindrome2("eye")); //true
+
+// ####  76. Telephone Number Validator  - freecodecamp
+/* 
+Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of a valid US number. The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false. */
+
+/* telephoneCheck("555-555-5555") should return a boolean.
+
+telephoneCheck("1 555-555-5555") should return true.
+
+telephoneCheck("1 (555) 555-5555") should return true.
+
++ telephoneCheck("5555555555") should return true. 
+
+telephoneCheck("555-555-5555") should return true.
+
+telephoneCheck("(555)555-5555") should return true.
+
+telephoneCheck("1(555)555-5555") should return true.
+
+telephoneCheck("555-5555") should return false.
+
+telephoneCheck("5555555") should return false.
+
+telephoneCheck("1 555)555-5555") should return false.
+
+telephoneCheck("1 555 555 5555") should return true.
+
+telephoneCheck("1 456 789 4444") should return true.
+
+telephoneCheck("123**&!!asdf#") should return false.
+
+telephoneCheck("55555555") should return false.
+
+telephoneCheck("(6054756961)") should return false.
+
+telephoneCheck("2 (757) 622-7382") should return false.
+
+telephoneCheck("0 (757) 622-7382") should return false.
+
+telephoneCheck("-1 (757) 622-7382") should return false.
+
+telephoneCheck("2 757 622-7382") should return false.
+
+telephoneCheck("10 (757) 622-7382") should return false.
+
+telephoneCheck("27576227382") should return false.
+
+telephoneCheck("(275)76227382") should return false.
+
+telephoneCheck("2(757)6227382") should return false.
+
+telephoneCheck("2(757)622-7382") should return false.
+
+telephoneCheck("555)-555-5555") should return false.
+
+telephoneCheck("(555-555-5555") should return false.
+
+telephoneCheck("(555)5(55?)-5555") should return false.
+
+telephoneCheck("55 55-55-555-5") should return false. */
+
+function telephoneCheck(str) {
+    if (/^1?([\s-])?\d{3}([\s-])?\d{3}([\s-])?\d{4}$/.test(str)) return true;
+
+    if (/^1?([\s-])?\(\d{3}\)([\s-])?\d{3}([\s-])?\d{4}$/.test(str))
+        return true;
+
+    return false;
+}
+
+// Combining the two Regex
+function telephoneCheck2(str) {
+    return /^1?([\s-])?(\d{3}|\(\d{3}\))([\s-])?\d{3}([\s-])?\d{4}$/.test(str);
+}
+console.log(telephoneCheck2("1-5555555555"));
+
+console.log(telephoneCheck2("555-555-5555"));
+console.log(telephoneCheck2("(555)555-5555"));
